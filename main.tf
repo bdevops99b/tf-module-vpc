5ce9f4e0d397875f15eca4267fad4dc5c83da883
bdevops99 <bdevops99@gmail.com>
2023-06-01 00:41:51 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -21,3 +21,21 @@ resource "aws_internet_gateway" "igw" {
 
   tags = merge(var.tags, { Name = "${var.env}-igw" })
 }
+
+resource "aws_eip" "ngw" {
+  count = length(lookup(lookup(var.subnets, "public", null), "cidr_block", 0))
+  vpc = true
+  tags = merge(var.tags, { Name = "${var.env}-ngw" })
+}
+#resource "aws_nat_gateway" "example" {
+#  allocation_id = aws_eip.example.id
+#  subnet_id     = aws_subnet.example.id
+#
+#  tags = {
+#    Name = "gw NAT"
+#  }
+#
+#  # To ensure proper ordering, it is recommended to add an explicit dependency
+#  # on the Internet Gateway for the VPC.
+#  depends_on = [aws_internet_gateway.example]
+#}
