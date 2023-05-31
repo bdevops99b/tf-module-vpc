1542350138efd01c8f412711006af9423d8a4a08
bdevops99 <bdevops99@gmail.com>
2023-06-01 00:26:56 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -15,3 +15,9 @@ module "subnets" {
   tags = var.tags
   env = var.env
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(var.tags, { Name = "${var.env}-igw" })
+}
