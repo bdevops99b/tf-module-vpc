f346da8e3e2632789754f58cd730d389f7487eae
bdevops99 <bdevops99@gmail.com>
2023-06-01 09:18:30 +0530

    Vpcs -subnets creations

--- subnets/main.tf
+++ subnets/main.tf
@@ -14,3 +14,9 @@ resource "aws_route_table" "main" {
   tags = merge(var.tags, { Name = "${var.env}-${var.name}-rt-${count.index+1}" })
 
 }
+
+resource "aws_route_table_association" "associate" {
+  count = length(var.cidr_block)
+  subnet_id      = aws_subnet.main[count.index].id
+  route_table_id = aws_route_table.main[count.index].id
+}
