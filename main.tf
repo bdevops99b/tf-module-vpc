542fa9204e73fbfa7cab5530c526291f4eb59f74
bdevops99 <bdevops99@gmail.com>
2023-06-04 14:15:22 +0530

    Vpcs -subnets creations

--- main.tf
+++ main.tf
@@ -55,14 +55,6 @@ resource "aws_vpc_peering_connection" "peer" {
   peer_vpc_id   = var.default_vpc_id
   vpc_id        = aws_vpc.main.id
   auto_accept = true
-
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
 }
 
 
